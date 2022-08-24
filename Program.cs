2874b2b030cf40b30db580d53aceb7c6a686df09
Gubbala-Sowmya <111357063+GubSowmya@users.noreply.github.com>
2022-08-24 22:51:59 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -23,3 +23,23 @@ public class HelloWorld
     }
 
 }
+
+// program -2
+
+int n1, n2;
+
+            Console.WriteLine("Enter the 1st number");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the 2st number");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            
+            int res = add(n1, n2);
+            Console.WriteLine("Sum of Two numbers is :" + res);
+        }
+        static int add(int i, int j)
+        {
+             return i + j;
+
+        }
