d3348ac96231f76ad3aa7654e76468e58def98c8
Gubbala-Sowmya <111357063+GubSowmya@users.noreply.github.com>
2022-08-24 22:56:42 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -163,3 +163,41 @@ employee emp = new employee();
             }
             
             // program -5
+            
+            //program - 6
+            
+            Console.WriteLine("Enter Department Name for number: ");
+                string dname = Console.ReadLine();
+                if (dname == "Purchase")
+                {
+                    int dep = (int)deptnames.Purchase;
+                    Console.WriteLine(dep);
+                }
+                else if (dname == "Sales")
+                {
+                    int dep = (int)deptnames.Sales;
+                    Console.WriteLine(dep);
+                }
+                else if (dname == "Training")
+                {
+                    int dep = (int)deptnames.Training;
+                    Console.WriteLine(dep);
+                }
+                else if (dname == "Accounts")
+                {
+                    int dep = (int)deptnames.Accounts;
+                    Console.WriteLine(dep);
+                }
+                else
+                    Console.WriteLine("not a department");
+                Console.ReadLine();
+            }
+            enum deptnames
+            {
+                Purchase = 1,
+                Sales = 2,
+                Training = 3,
+                Accounts = 4
+            }
+            
+// program - 7
