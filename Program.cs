f075b011a2a2c5be932f686482349f204c40bc24
Gubbala-Sowmya <111357063+GubSowmya@users.noreply.github.com>
2022-08-24 22:55:22 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -118,3 +118,48 @@ employee emp = new employee();
             // program -4
             
             
+            Employee emp = new Employee();
+
+            Console.WriteLine("Enter Employee number");
+            emp.eno = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee name");
+            emp.ename = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee salary");
+            emp.esal = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee department number");
+            emp.edeptno = Convert.ToInt32(Console.ReadLine());
+
+            emp.AcceptData(int eno, string ename, int esal, int edeptno);
+            Console.WriteLine("Employee details are : ");
+            emp.Display();
+            Console.ReadLine();
+
+
+
+        }
+
+        struct Employee
+        {
+            int Empnum, Esal, Edept;
+            string EmpName;
+            public static AcceptData(int empno, string ename, int sal, int deptno)
+            {
+                Empnum = empno;
+                EmpName = ename;
+                Esal = sal;
+                Edept = deptno;
+                
+            }
+            public static Display()
+            {
+                Console.WriteLine("Employee number is :", +Empnum);
+                Console.WriteLine("Employee Name is :", +EmpName);
+                Console.WriteLine("Employee salary is :", +Esal);
+                Console.WriteLine("Employee Dept number is :", +Edept);
+
+            }
+            
+            // program -5
