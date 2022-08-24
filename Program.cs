1617e263d42850a7c2fd822c0aba4c4d04fdbdcc
Gubbala-Sowmya <111357063+GubSowmya@users.noreply.github.com>
2022-08-24 22:53:53 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -43,3 +43,78 @@ int n1, n2;
              return i + j;
 
         }
+
+
+// program -3
+
+employee emp = new employee();
+
+            Console.WriteLine("Enter employee number :");
+            emp.a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee name :");
+            emp.b = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee Salary :");
+            emp.c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee Dept Number :");
+            emp.d = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Employee Details are:");
+            emp.DisplayInfo();
+            Console.ReadLine();
+
+
+
+
+
+        }
+        struct employee
+        {
+            int _empno;
+            public int a
+            {
+                get
+                {
+                    return _empno;
+                }
+                set
+                {
+                    _empno = value;
+                }
+            }
+
+            string _ename;
+            public string b
+            {
+                get { return _ename; }
+                set { _ename = value; }
+            }
+
+            int _sal;
+            public int c
+            {
+                get { return _sal; }
+                set { _sal = value; }
+            }
+
+            int _deptno;
+            public int d
+            {
+                get { return _deptno; }
+                set { _deptno = value; }
+            }
+
+            public void DisplayInfo()
+            {
+                Console.WriteLine(_empno);
+                Console.WriteLine(_ename);
+                Console.WriteLine(_sal);
+                Console.WriteLine(_deptno);
+
+            }
+        
+            // program -4
+            
+            
